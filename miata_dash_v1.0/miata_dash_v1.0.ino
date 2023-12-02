3e17480ee4fee8953fbf408f0eab221b17d565b5
Isai <isaiperez63@gmail.com>
2023-12-01 23:53:35 -0800

    changed pin layout

--- miata_dash_v1.0/miata_dash_v1.0.ino
+++ miata_dash_v1.0/miata_dash_v1.0.ino
@@ -28,36 +28,36 @@ float frq;
 // **** Gauges **** 4
 // Max Voltage for the analog inputs should be 0 - 3.75V.
 
-int Fuel = A0; // Analog signal will be used to read the level of the gas tank
-int TEMP = A1; // Analog
-int Tack ; // Analog signal will come in throug D5
-int Oil_Temp = A4;// Analog
+int Fuel = A0;      // Analog signal will be used to read the level of the gas tank
+int TEMP = A1;      // Analog
+int Tack;           // Analog signal will come in throug D5
+int Oil_Temp = A4;  // Analog
 
 //  **** Top Center **** 2
-int TurnLeft = 23; // Top Center
-int TurnRight = 24; // Top Center
-int Ground  ;
+int TurnLeft = 26;   // Top Center
+int TurnRight = 27;  // Top Center
+int Ground;
 
 //
-int HOLD = 25;
-int AirBag = 26; // On Tack Gauge
-int Retract = 27; // On Tack Gauge
+int HOLD = 35;
+int AirBag = 24;   // On Tack Gauge
+int Retract = 31;  // On Tack Gauge
 
 
 // **** Middle Center **** 7
-int ABS = 28; //Middle Center
-int Washer = 29; // Middle Center
-int Beam = 30; // Middle Center
-int Belts = 31; // Middle Center
-int Break = 32; // Middle Center
-int Charge = 33;  // Middle Center
-int Check_Engine = 34; // Middle Center
+int ABS = 28;           //Middle Center
+int Washer = 29;        // Middle Center
+int Beam = 33;          // Middle Center
+int Belts = 25;         // Middle Center
+int Break = 23;         // Middle Center
+int Charge = 22;        // Middle Center
+int Check_Engine = 29;  // Middle Center
 
 
-int Read_Fuel =50 ;
+int Read_Fuel = 50;
 
 
-Smoothed <int> tempSensor;
+Smoothed<int> tempSensor;
 
 // ........................................Some Globals...................................................
 // Array used to values for testing * Will need to delete
@@ -68,8 +68,7 @@ const int capacity = JSON_OBJECT_SIZE(18);
 StaticJsonDocument<capacity> carData;
 
 
-void setup()
-{
+void setup() {
   Serial.begin(115200);
 
   // For testing, uncomment one of these lines and connect
@@ -110,8 +109,7 @@ void setup()
   tempSensor.begin(SMOOTHED_EXPONENTIAL, 50);
 }
 
-void loop()
-{
+void loop() {
   // Read_Fuel = analogRead(Fuel);
   // Serial.print(Read_Fuel);
   // tempSensor.add(Read_Fuel);
@@ -120,16 +118,15 @@ void loop()
   // stop Timer 0 interrupts from throwing the count out
   byte oldTCCR0A = TCCR0A;
   byte oldTCCR0B = TCCR0B;
-  TCCR0A = 0;    // stop timer 0
+  TCCR0A = 0;  // stop timer 0
   TCCR0B = 0;
 
   startCounting(500);  // how many ms to count for
 
-  while (!counterReady)
-  { }  // loop until count over
+  while (!counterReady) {}  // loop until count over
 
   // adjust counts by counting interval to give frequency in Hz
-  frq = (timerCounts *  1000.0) / timerPeriod;
+  frq = (timerCounts * 1000.0) / timerPeriod;
 
 
   // restart timer 0
@@ -139,5 +136,4 @@ void loop()
   // let serial stuff finish
   //  delay(200);
   checkLights();
-
 }
