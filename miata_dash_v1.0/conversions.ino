268596d822cf54bb05b6156f835d84c00d14ad5e
Isai <isaiperez63@gmail.com>
2023-12-03 18:01:57 -0800

    changed formula for voltage conversions

--- miata_dash_v1.0/conversions.ino
+++ miata_dash_v1.0/conversions.ino
@@ -6,21 +6,27 @@ float analogToVoltage(float analog){
 
 // This function takes in a voltage and returns the amount
 // of gallons which are equivalent ot the voltage given
-float voltageToGallons(float voltage){
+float voltageToGallons(float analog){
+  float voltage = analogToVoltage(analog);
   return voltage / 0.991;
 }
 
 // This function takes in a voltage and returns the temperature 
 //equivalent ot the voltage given
-float voltageToTemp(float voltage){
-  return voltage / 0.991;
+float voltageToTemp(float analog){
+  float voltage = analogToVoltage(analog);
+  float normal_operating_temp = 195;
+  float voltage_at_norm_op_temp = 5.13;
+  float conversion_rate = (normal_operating_temp * .01)/voltage_at_norm_op_temp; //0.380116959
+
+  
+  return (voltage * 100) * conversion_rate;
 }
 
 // This function takes in a voltage and returns the PSI
 // equivalent ot the voltage given
 // This is for the oil pressure gauge
-float voltageToPreassure(float voltage){
+float voltageToPreassure(float analog){
+  float voltage = analogToVoltage(analog);
   return voltage / 0.991;
 }
-
-
